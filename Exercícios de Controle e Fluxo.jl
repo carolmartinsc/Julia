e23ec6c0af98e28bb75283715c214cda80378d01
carolmartinsc <carolmartinscrispim@gmail.com>
2020-03-27 23:09:58 -0300

    alguns exercícios

---
+++ Exercícios de Controle e Fluxo.jl
@@ -0,0 +1,20 @@
+###################### Exercícios de Controle e Fluxo e Funções #############################
+function tipo_triangulo(a,b,c)
+    if a==b & a==c & b==c
+        return "Triângulo Equilátero"
+    elseif a==b || a==c || c==b
+        return "Triângulo Isósceles"
+    else
+        return "Triângulo Escaleno"
+    end    
+end
+tipo_triangulo(3,4,3)
+
+if i % 3 == 0
+    x=[x ; i]
+
+
+
+
+
+
